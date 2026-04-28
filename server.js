c124d62f09247c0dad62942ea21f8219cd0665bf
clinicaduqueesilva-ai <clinicaduqueesilva@gmail.com>
2026-04-28 19:38:05 -0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("WEBHOOK ONLINE");
+});
+
+app.post("/api/webhooks/whatsapp", (req, res) => {
+  console.log("🔥 WEBHOOK RECEBIDO:");
+  console.log(req.body);
+  res.sendStatus(200);
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server rodando na porta ${PORT}`);
+});
