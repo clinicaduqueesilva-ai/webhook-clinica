b50a18e603d487a744d05cec11f644edd8e70bdc
clinicaduqueesilva-ai <clinicaduqueesilva@gmail.com>
2026-04-28 19:22:47 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,9 +3,13 @@ import express from "express";
 const app = express();
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("WEBHOOK ONLINE");
+});
+
 app.post("/api/webhooks/whatsapp", (req, res) => {
   console.log("🔥 WEBHOOK RECEBIDO:");
-  console.log(req.body);
+  console.log(JSON.stringify(req.body, null, 2));
 
   res.sendStatus(200);
 });
